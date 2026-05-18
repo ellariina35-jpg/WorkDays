0941aa7860b45f79ebadc95006c075ec8d69048c
ellariina35-jpg <ellariina35@gmail.com>
2026-05-18 13:28:34 +0300

    Add project files.

---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace WorkDays
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
